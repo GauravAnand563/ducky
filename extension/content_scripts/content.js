e89494f5a7cc04f5c747e701fa4a2502b0ea8505
GauravAnand563 <gauravanand563@gmail.com>
2023-02-22 00:00:56 +0530

    add tooltip to ducky

--- extension/content_scripts/content.js
+++ extension/content_scripts/content.js
@@ -2,15 +2,16 @@ const script = document.createElement('script');
 script.src = chrome.runtime.getURL('lib/lottie.min.js');
 document.head.appendChild(script);
 
-
 const duckyDiv = document.createElement("div");
 duckyDiv.className = "ducky ducky-is-dead";
 duckyDiv.innerHTML = `
-<div id="duck"></div>
+<div id="duck">
+    <div class="tooltip">Debug?</div>
+</div>
 `;
 document.body.appendChild(duckyDiv);
 
-var animation = lottie.loadAnimation({
+const animation = lottie.loadAnimation({
     container: document.getElementById('duck'),
     renderer: 'svg',
     loop: true,
@@ -18,11 +19,9 @@ var animation = lottie.loadAnimation({
     path: chrome.runtime.getURL("animation_files/ducky_hi.json")
 });
 document.addEventListener("selectionchange", function () {
-    // const ducky = document.querySelector(".ducky");
     duckyDiv.classList.toggle("ducky-is-dead");
     const selection = window.getSelection();
     if (selection.toString()) {
-        console.log("selection", selection.toString());
         if (duckyDiv.classList.contains("ducky-is-dead")) {
             duckyDiv.classList.remove("ducky-is-dead");
         }
